755e039df3e5bc541ea0acd33b2ff2b97740098b
daegwonjeon@TIP209 <james911256@gmail.com>
2023-09-21 10:15:30 +0900

    원 운동 완성

--- move_character.py
+++ move_character.py
@@ -6,7 +6,14 @@ grass = load_image("grass.png")
 character = load_image("character.png")
 
 def run_circle():
-    pass
+    cx, cy, r = 400, 300, 200
+    for deg in range(0,360,1):
+        x = cx + r*math.cos(math.radians(deg))
+        y = cy + r*math.sin(math.radians(deg))
+        clear_canvas_now()
+        grass.draw_now(400,30)
+        character.draw_now(x,y)
+        delay(0.01)
 
 def run_rectangle():
     pass
@@ -14,5 +21,7 @@ def run_rectangle():
 while True:
     run_circle()
     run_rectangle()
+    break
+
 
 close_canvas()
