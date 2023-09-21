1378b20027a6a5078ac78ca925850e49c215d278
daegwonjeon@TIP209 <james911256@gmail.com>
2023-09-21 10:03:59 +0900

    함수 정의 및 루프 작성

---
+++ move_character.py
@@ -0,0 +1,18 @@
+from pico2d import *
+
+open_canvas()
+
+grass = load_image("grass.png")
+character = load_image("character.png")
+
+def run_circle():
+    pass
+
+def run_rectangle():
+    pass
+
+while True:
+    run_circle()
+    run_rectangle()
+
+close_canvas()
